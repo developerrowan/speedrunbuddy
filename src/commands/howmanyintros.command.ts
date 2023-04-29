118a5743e104b6c6e093fe7f51a24610991b4dd2
Devin Rowan <its@autumnrowan.com>
2023-04-28 22:43:11 -0500

    Add !howmanyintros

---
+++ src/commands/howmanyintros.command.ts
@@ -0,0 +1,52 @@
+import { RunHistory, UserProfileRun, getHistory } from 'therungg';
+import {
+  ClientWrapper,
+  CommandProperties,
+} from '../services/command-dispatch.service';
+import Speedrunbuddy from '../speedrunbuddy';
+import ICommand from './ICommand';
+import { TheRunService, UtilityService } from '../services';
+import { Channel } from '../services/channel.service';
+
+export default class HowManyIntrosCommand implements ICommand {
+  public name = 'howmanyintros';
+  public alias = 'hmi';
+
+  public async execute(
+    wrapper: ClientWrapper,
+    properties: CommandProperties
+  ): Promise<void> {
+    const channel: Channel = wrapper.channel;
+
+    const run: UserProfileRun | undefined = await TheRunService.negotiateRun(
+      wrapper,
+      properties
+    );
+
+    if (!run || !run.game.includes('Super Mario 64')) return;
+
+    const category: string = UtilityService.splitHash(run.displayRun);
+
+    const introTime = 55000;
+    let totalIntroWatched = 0;
+
+    const history: RunHistory = await getHistory(run.historyFilename);
+
+    for (let i = 0; i < history.runs.length; i++) {
+      const time: number = +history.runs[i].time;
+
+      if (time > introTime) {
+        totalIntroWatched += time - introTime;
+      }
+    }
+
+    Speedrunbuddy.client.say(
+      channel.ircChannelName,
+      `${
+        channel.displayName
+      } has spent ${UtilityService.millisecondsToDaysHourMinutes(
+        totalIntroWatched
+      )} watching the intro while running ${category} (calculated assuming intro is exactly 55 seconds long).`
+    );
+  }
+}
